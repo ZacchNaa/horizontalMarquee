6378baac94c69d6f27796cf2caaf27e9e2d090e8
ZacchNaa <53350754+ZacchNaa@users.noreply.github.com>
2022-01-09 12:12:21 +0000

    scroll jacking

--- script.js
+++ script.js
@@ -1,28 +1,99 @@
-const wrappers = document.querySelectorAll(`wrapper`);
-const wrapperOne = document.querySelectorAll(`wrapperOne`);
+// const dots = document.querySelectorAll(".dot");
+const sections = document.querySelectorAll(".section");
+const sectionContainer = document.querySelector(".scrolljacking");
+// const loader = document.querySelector("#js-loader");
 
-const data = [
-    {id:1, title:`lable 1`},
-    {id:2, title:`lable 2`},
-    {id:3, title:`lable 3`},
-    {id:4, title:`lable 4`},
-    {id:5, title:`lable 5`},
-    {id:6, title:`lable 6`},
-    {id:7, title:`lable 7`},
-    {id:8, title:`lable 8`},
-    {id:9, title:`lable 9`},
-    {id:10, title:`lable 10`},
-]
+// For tracking the section currently displayed
+let currentSection = 0;
+let canScroll = true;
 
-const createDivHandler = () => {
-    data.forEach((element) => {
-			const newDiv = document.createElement(`div`);
-			newDiv.classList.add(`item`);
-			newDiv.innerText = `${element.title}`;
-            document.body.appendChild(newDiv);
-			// wrappers.forEach((wrapper) => {
-			// });
-		});
+// Touch data (for mobile)
+let touchStart = 0;
+let touchEnd = 0;
+
+// First, let's put some content in all the sections
+// sections.forEach((section, index) => {
+// 	fetch("https://baconipsum.com/api/?type=meat-and-filler&sentences=4")
+// 		.then((blob) => {
+// 			return blob.json();
+// 		})
+// 		.then(function (data) {
+// 			loader.style.display = "none";
+// 			section.innerHTML = `<h2>Section: ${index + 1}</h2><p>${data}</p>`;
+// 			// set the first dot to be active.
+// 			if (0 == index) {
+// 				setDotIndicator();
+// 			}
+// 		});
+// });
+
+// // set event listeners on the dots
+// dots.forEach((dot, index) => {
+// 	dot.addEventListener("click", (event) => {
+// 		resetDotIndicator();
+// 		currentSection = index;
+// 		sectionContainer.style.top = currentSection * -100 + "vh";
+// 		setDotIndicator();
+// 	});
+// });
+
+document.addEventListener("touchstart", (event) => {
+	touchStart = event.changedTouches[0].clientY;
+});
+
+document.addEventListener("touchend", (event) => {
+	touchEnd = event.changedTouches[0].clientY;
+	if (touchStart > touchEnd) {
+		performScroll(1);
+	} else {
+		performScroll(-1);
+	}
+});
+
+// Add event listener for scrolling, so we can do our scroll jacking
+document.addEventListener("wheel", (event) => {
+	// Check if we can scroll yet, or if we're in the middle of a 
+    // scroll jack.
+	// If so, prevent all scrolling
+	if (!canScroll) {
+		return;
+	}
+
+	// Not in a scroll, so set a timeout for when scrolling can happen next
+	canScroll = false;
+	setTimeout(() => {
+		canScroll = true;
+	}, 15000);
+
+	// Perform the scroll to the next section
+	// First, get the next section to scroll to by checking if the scroll position is greater or less than the position of the current position.
+	scrollDir = event.deltaY > 1 ? 1 : -1;
+	performScroll(scrollDir);
+});
+
+function performScroll(scrollDir) {
+	// resetDotIndicator();
+	currentSection += scrollDir;
+
+	// Make sure we stick to real sections and don't scroll out of it.
+	if (currentSection >= sections.length - 1) {
+		currentSection = sections.length - 1;
+	}
+	if (0 > currentSection) {
+		currentSection = 0;
+	}
+
+	// perform the 'scroll'
+	sectionContainer.style.top = currentSection * -100 + "vh";
+	// setDotIndicator();
 }
 
-createDivHandler()
+// reset the old dot to not be active
+// function resetDotIndicator() {
+// 	dots[currentSection].classList.remove("current-dot");
+// }
+
+// // Set the current dot displayed
+// function setDotIndicator() {
+// 	dots[currentSection].classList.add("current-dot");
+// }
